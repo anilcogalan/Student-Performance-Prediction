383dd402d6b44b818fbd43c40be9acc374b65730
Anil <nilc@outlook.com>
2024-05-22 15:30:15 +0300

    Data Ingestion&Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,33 +1,28 @@
 import os
 import sys
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(current_dir)
-print(sys.path)
-from src.exception import CustomException
-from src.logger import logging
 import pandas as pd
-
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.exception import CustomException
+from src.logger import logging
 
-
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('artifacts','train.csv')
-    test_data_path: str=os.path.join('artifacts','test.csv')
-    raw_data_path: str=os.path.join('artifacts','data.csv')
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    test_data_path: str = os.path.join('artifacts', 'test.csv')
+    raw_data_path: str = os.path.join('artifacts', 'data.csv')
 
 class DataIngestion:
-    def __init__(self, config: DataIngestionConfig):
-        self.config = config
+    def __init__(self):
+        self.config = DataIngestionConfig()
 
     def load_data(self):
         try:
             logging.info("Loading data from raw data path...")
-            df = pd.read_csv('data/data.csv')
+            df = pd.read_csv("D:\Software\Portfolio\Student_Performance_Prediction\data\data.csv")
 
             logging.info('Reading data from raw data path is successful!')
             os.makedirs(os.path.dirname(self.config.train_data_path), exist_ok=True)
@@ -39,20 +34,17 @@ class DataIngestion:
             test_set.to_csv(self.config.test_data_path, index=False, header=True)
 
             logging.info("Ingestion of data is successful!")
-
-            return(
-                self.config.train_data_path,
-                self.config.test_data_path
-            )
+            return (self.config.train_data_path, self.config.test_data_path)
 
         except Exception as e:
-            raise CustomException(e, sys)
-
+            exc_type, exc_value, exc_traceback = sys.exc_info()  # Correct way to capture traceback
+            raise CustomException(e, exc_traceback)  # Pass traceback object
 
 if __name__ == '__main__':
-    config = DataIngestionConfig()
-    data_ingestion = DataIngestion(config)
-    data_ingestion.load_data()
+    obj = DataIngestion()
+    train_data, test_data = obj.load_data()
 
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
 
 
