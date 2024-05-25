ab64848bb0f4acfb950e6e68c7b77067eaffbef9
Anil <nilc@outlook.com>
2024-05-25 22:14:53 +0300

    Prediction Pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,11 +9,14 @@ from src.logger import logging
 
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str = os.path.join('artifacts', 'train.csv')
-    test_data_path: str = os.path.join('artifacts', 'test.csv')
-    raw_data_path: str = os.path.join('artifacts', 'data.csv')
+    train_data_path: str = os.path.join('../../artifacts', 'train.csv')
+    test_data_path: str = os.path.join('../../artifacts', 'test.csv')
+    raw_data_path: str = os.path.join('../../artifacts', 'data.csv')
 
 class DataIngestion:
     def __init__(self):
@@ -40,11 +43,12 @@ class DataIngestion:
             exc_type, exc_value, exc_traceback = sys.exc_info()  # Correct way to capture traceback
             raise CustomException(e, exc_traceback)  # Pass traceback object
 
-if __name__ == '__main__':
+if __name__=="__main__":
     obj = DataIngestion()
     train_data, test_data = obj.load_data()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data, test_data)
 
-
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -20,32 +20,31 @@ from src.logger import logging
 from src.utils import save_object, evaluate_models
 
 @dataclass
-class ModelTraninerConfig:
-    trained_model_file_path = os.path.join("artifacts", "trained_model.pkl")
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join("../../artifacts", "model.pkl")
+
 
 class ModelTrainer:
     def __init__(self):
-        self.model_trainer_config = ModelTraninerConfig()
+        self.model_trainer_config = ModelTrainerConfig()
 
-
-    def initiate_model_trainer(self,train_arr,test_arr,preprocessor_path):
+    def initiate_model_trainer(self, train_array, test_array):
         try:
             logging.info("Split training and test input data")
-            X_train,y_train,X_test,y_test = (
-                train_arr[:,:-1],
-                train_arr[:,-1],
-                test_arr[:,:-1],
-                test_arr[:,-1]
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
             )
             models = {
-                "LinearRegression": LinearRegression(),
-                "RandomForestRegressor": RandomForestRegressor(),
-                "DecisionTreeRegressor": DecisionTreeRegressor(),
-                "KNeighborsRegressor": KNeighborsRegressor(),
-                "AdaBoostRegressor": AdaBoostRegressor(),
-                "GradientBoostingRegressor": GradientBoostingRegressor(),
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
                 "XGBRegressor": XGBRegressor(),
-                "CatBoostRegressor": CatBoostRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
             }
             params = {
                 "Decision Tree": {
@@ -85,31 +84,39 @@ class ModelTrainer:
 
             }
 
-            model_report:dict=evaluate_models(X_train, y_train, X_test, y_test, models, params)
+            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,
+                                                 models=models, param=params)
 
+            ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
 
-            best_model_name = [key for key in model_report if model_report[key] == best_model_score][0]
+            ## To get best model name from dict
 
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
             best_model = models[best_model_name]
 
-            if best_model_score <0.6:
-                raise CustomException("Model score is less than 0.6",sys)
-            logging.info(f"Best Model: {best_model_name} with score: {best_model_score}")
+            if best_model_score < 0.6:
+                raise CustomException("No best model found")
+            logging.info(f"Best found model on both training and testing dataset")
 
             save_object(
-                file_path= self.model_trainer_config.trained_model_file_path,
+                file_path=self.model_trainer_config.trained_model_file_path,
                 obj=best_model
             )
 
             predicted = best_model.predict(X_test)
 
-            r2_square = r2_score(y_test,predicted)
-
+            r2_square = r2_score(y_test, predicted)
             return r2_square
 
+
+
+
+
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e, sys)
 
 
 
